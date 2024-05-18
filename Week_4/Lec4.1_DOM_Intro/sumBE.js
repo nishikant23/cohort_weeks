5327dd3caa416b079afaa387c4bf005b2291dda0
Nishikant R Dounekar <119045534+nishikant23@users.noreply.github.com>
2024-05-18 17:29:47 +0530

    Debouncing add

--- Week_4/Lec4.1_DOM_Intro/sumBE.js
+++ Week_4/Lec4.1_DOM_Intro/sumBE.js
@@ -1,26 +1,26 @@
-    const express = require("express");
-    const cors =require("cors");
-    const app = express();
-    app.use(express.json());
-    app.use(cors());
-    app.listen(3013)
-    
+const express = require("express");
+const cors =require("cors");
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.listen(3013)
 
-    app.get("/sum", (req, res) => {
 
-        const a = req.query.a
-        const b = req.query.b
-        if(!a || !b){
-            return res.status(400).send("Both a and b values are required")
-        }
+app.get("/sum", (req, res) => {
 
-        const num1 = parseInt(a)
-        const num2 = parseInt(b)
-        if(isNaN(num1) || isNaN(num2)) {
-            return res.status(400).send("Invalide numeric values either of a or b or both")
-        }
+    const a = req.query.a
+    const b = req.query.b
+    if(!a || !b){
+        return res.status(400).send("Both a and b values are required")
+    }
 
-        const sum = num1 + num2
-       // res.setHeader("Content-Type", "text/plain");
-        return res.json(`${sum}`)
-    })
+    const num1 = parseInt(a)
+    const num2 = parseInt(b)
+    if(isNaN(num1) || isNaN(num2)) {
+        return res.status(400).send("Invalide numeric values either of a or b or both")
+    }
+
+    const sum = num1 + num2
+   // res.setHeader("Content-Type", "text/plain");
+    return res.json(`${sum}`)
+})
